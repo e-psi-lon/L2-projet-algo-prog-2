1ce515312cdc127ac61014f3afb26ac3ba07a93f
Natskil <nels.dacosta@hotmail.fr>
2026-04-26 16:43:28 +0200

    mise en place des méthodes de tests

--- src/main/java/dev/e_psi_lon/Main.java
+++ src/main/java/dev/e_psi_lon/Main.java
@@ -50,19 +50,104 @@ public class Main {
         System.out.println(a.haversineDistance(b));
         */
 
-        Graphe g = new Graphe("52-points (avec-points).csv");
+        /*Graphe g = new Graphe("100_nodes_positive.csv");
 
         System.out.println("\nGlouton :");
 
         g.glouton();
-        System.out.println("Poids = " +g.getTotalWeight());
+        System.out.println("Poids = " +g.getTotalWeight());*/
 
-        System.out.println("\nMST :");
+        /*System.out.println("\nMST :");
 
-        Graphe g2 = new Graphe("14-points (avec point et non virgule pour les doubles).csv");
+        Graphe g2 = new Graphe("52-points (avec-points).csv");
         g2.construireGrapheComplet();
         g2 = g2.mst();
-        System.out.println("Poids = " + g2.getTotalWeight());
-        
+        System.out.println("Poids = " + g2.getTotalWeight());*/
+
+        /*Graphe g3 = new Graphe("100_nodes_positive.csv");
+        g3.construireGrapheComplet();
+        g3 = g3.mm();
+        System.out.println("Poids = " + g3.getTotalWeight());*/
+
+    int repetitions = 1;
+
+/*//GLOUTON
+    double minG = Double.MAX_VALUE;
+    double maxG = Double.MIN_VALUE;
+    long totalTimeG = 0;
+
+    for (int i = 0; i < repetitions; i++) {
+
+        Graphe g = new Graphe("1000_noeuds.csv");
+
+        long startG = System.nanoTime();
+
+        g.glouton();
+
+        long endG = System.nanoTime();
+
+        double poidsG = g.getTotalWeight();
+
+        if (poidsG < minG) minG = poidsG;
+        if (poidsG > maxG) maxG = poidsG;
+
+        totalTimeG += (endG - startG);
+    }
+
+    System.out.println("===== Glouton =====");
+    System.out.println("MIN G = " + minG);
+    System.out.println("MAX G = " + maxG);
+    System.out.println("Temps moyen G (ms) = " + (totalTimeG / (double) repetitions / 1_000_000)+"\n");*/
+
+
+    //mst
+    long totalTimeS = 0;
+    double poidsS = 0;
+
+    for (int i = 0; i < repetitions; i++) {
+
+        Graphe s = new Graphe("1000_noeuds.csv");
+
+        s.construireGrapheComplet();
+
+        long startS = System.nanoTime();
+
+        s = s.mst();
+
+        long endS = System.nanoTime();
+
+        poidsS = s.getTotalWeight();
+
+        totalTimeS += (endS - startS);
+    }
+
+    System.out.println("\n===== MST =====");
+    System.out.println("Poids total : "+ poidsS);
+    System.out.println("Temps moyen S (ms) = " + (totalTimeS / (double) repetitions / 1_000_000)+"\n");
+
+
+//MM
+    long totalTimeM = 0;
+    double poidsM = 0;
+
+    for (int i = 0; i < repetitions; i++) {
+
+        Graphe m = new Graphe("1000_noeuds.csv");
+
+        m.construireGrapheComplet();
+
+        long startM = System.nanoTime();
+
+        m = m.mm();
+
+        long endM = System.nanoTime();
+
+        poidsM = m.getTotalWeight();
+        totalTimeM += (endM - startM);
+    }
+
+    System.out.println("\n===== MM =====");
+    System.out.println("Poids total : "+ poidsM);
+    System.out.println("Temps moyen M (ms) = " + (totalTimeM / (double) repetitions / 1_000_000));
     }
 }
